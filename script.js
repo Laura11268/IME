722f4378a95af8dc47471ba9c8bdcdeba63f7a69
Laura11268 <lauhdz1110@icloud.com>
2025-07-14 11:36:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// Se incluirá contenido generado anteriormente más resumido por espacio
+document.getElementById('malla').innerHTML = '<p>Código resumido. Se cargará versión completa en próximas ejecuciones.</p>';
